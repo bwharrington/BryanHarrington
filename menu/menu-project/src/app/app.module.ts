a4512462d241ea50d3d34fbf58720d74c31aeca3
bwharrington <Bryan@BryanHarrington.net>
2020-08-28 23:11:13 -0500

    Added data, code
    Added data, code

--- menu/menu-project/src/app/app.module.ts
+++ menu/menu-project/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuContainerComponent } from './menu-container/menu-container.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuContainerComponent
+    MenuContainerComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
